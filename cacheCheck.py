c99d4706275302fab78c1581a57f71e630f34d7f
heamon7 <heamon7@gmail.com>
2015-05-23 22:23:17 +0800

    add quesInfo tool

---
+++ cacheCheck.py
@@ -0,0 +1,8 @@
+import bmemcached
+client1 = bmemcached.Client('d69c4508ccc94dc4.m.cnbjalinu16pub001.ocs.aliyuncs.com:11211','d69c4508ccc94dc4','Zhihucache1')
+
+import bmemcached
+client2 = bmemcached.Client('7030b81da1324743.m.cnbjalinu16pub001.ocs.aliyuncs.com:11211','7030b81da1324743','Zhihucache2')
+
+import bmemcached
+client3 = bmemcached.Client('92a2b309a9f145d2.m.cnbjalinu16pub001.ocs.aliyuncs.com:11211','92a2b309a9f145d2','Zhihucache3')
---
+++ quesInfo/quesInfo.py
@@ -0,0 +1,35 @@
+
+import bmemcached
+import numpy as np
+import csv
+from datetime import datetime
+import re
+
+client2 = bmemcached.Client('7030b81da1324743.m.cnbjalinu16pub001.ocs.aliyuncs.com:11211','7030b81da1324743','Zhihucache2')
+client3 = bmemcached.Client('92a2b309a9f145d2.m.cnbjalinu16pub001.ocs.aliyuncs.com:11211','92a2b309a9f145d2','Zhihucache3')
+dbPrime = 97
+questionIdSet=set()
+questionInfoList =[]
+
+totalCount = int(client2.get('totalCount'))
+print "totalCount: %s\n" %str(totalCount)
+
+for questionIndex in range(0,5+1):
+    questionIdSet.add(int(client2.get(str(questionIndex))[0]))
+
+print "length of  questionIdSet: %s" %str(len(questionIdSet))
+
+for questionId in questionIdSet:
+    res = np.array(client3.get(str(questionId)))[[1,3,4]]
+    questionInfoList.append(np.insert(res,0,int(questionId)))
+
+with open('/home/heamon7/Project/localDB.csv','w') as localDB:
+    fieldnames = ['questionId','isTopQuestion','questionAnswerCount','questionFollowerCount']
+    questionInfoWriter = csv.writer(localDB,delimiter=' ')
+    for questionInfo in questionInfoList:
+        questionInfoWriter.writerow(questionInfo)
+
+
+
+
+print 'Finished All'
