780ac7ce54e8bf21f57029fb8f531aab8a0f605f
heamon7 <heamon7@gmail.com>
2015-05-25 00:56:32 +0800

    update cache question

--- cache/cache.py
+++ cache/cache.py
@@ -8,7 +8,7 @@ import re
 
 leancloud.init('8scc82ncveedyt6p8ilcz2auzoahzvpu2y800m5075f9flp9', master_key='06vseo6z44ummz0fgv0u6no7vnzqr4fbob0y2mxz6cv47p92')
 client1 = bmemcached.Client(('aa41ddf13b914084.m.cnbjalinu16pub001.ocs.aliyuncs.com:11211'),'aa41ddf13b914084','Zhihu7771')
-client2 = bmemcached.Client(('aa41ddf13b914084.m.cnbjalinu16pub001.ocs.aliyuncs.com:11211'),'aa41ddf13b914084','Zhihu7771')
+client2 = bmemcached.Client(('b2954ece3d1647b8.m.cnbjalinu16pub001.ocs.aliyuncs.com:11211'),'b2954ece3d1647b8','Zhihu7772')
 dbPrime = 97
 
 totalQuestion=0
